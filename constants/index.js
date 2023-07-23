5cae49118203fe7d8138dd220fa5a41d6b047b60
HessamRisky <121561245+HessamRisky-NEW@users.noreply.github.com>
2023-07-23 16:53:33 +0000

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xa5eda56ddebc3938cc3abeef43bdead8f8f15f89";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
